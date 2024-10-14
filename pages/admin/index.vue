f07d113ff09f9cb4d8102153d163a01ed6fb504c
Emmanuel687 <koechemmanuel2002@gmail.com>
2024-10-14 21:32:55 +0300

    Chore:-Created Dashboard

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,5 +1,30 @@
 <template>
-  Analytics Section
+  <section class="dashboard rounded-md py-3 bg-white">
+    <div class="grid-cols-1 lg:grid-cols-2 gap-3" style="display: grid">
+      <!-- All Task Table Start -->
+      <AdminDashboardAllTasks />
+      <!-- All Task Table End -->
+
+      <!-- Pending Task Table Start -->
+      <AdminDashboardPendingTasks />
+      <!-- Pending Task Table End -->
+
+      <!-- Completed Tasks Table Start -->
+      <AdminDashboardCompletedTasks />
+      <!-- Completed Tasks Table End -->
+
+      <!-- Charts Start-->
+      <AdminDashboardChartsAnalytics />
+      <!-- Charts Start-->
+    </div>
+
+
+
+
+
+
+
+  </section>
 </template>
 
 <script setup>
