134e358da7ffc6142c8868587aeb3c002bd9185f
Emmanuel687 <koechemmanuel2002@gmail.com>
2024-10-14 14:25:42 +0300

    Chore:-Changed LogOut button Colour

--- components/admin/AdminSidebar.vue
+++ components/admin/AdminSidebar.vue
@@ -60,7 +60,7 @@
       </nav>
       <div class="p-4">
         <button @click="handleLogout"
-          class="w-full flex items-center justify-center space-x-2 px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition duration-200">
+          class="w-full flex items-center justify-center space-x-2 px-4 py-2 bg-white text-indigo-600 rounded  transition duration-200 border-indigo-600 border">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
