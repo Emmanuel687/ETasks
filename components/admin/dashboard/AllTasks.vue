c40b91f3d97b7e4807bc72252f4547fcb7a370c1
Emmanuel687 <koechemmanuel2002@gmail.com>
2024-10-14 18:00:20 +0300

    Chore:-Created Dashboard view

---
+++ components/admin/dashboard/AllTasks.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+
+const products = ref([
+  {
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+  },
+])
+
+</script>
+
+<template>
+  <div>
+    Pending Tasks
+    <DataTable :value="products" tableStyle="min-width: 20rem">
+      <Column field="code" header="Code"></Column>
+      <Column field="name" header="Name"></Column>
+      <Column field="category" header="Category"></Column>
+      <Column field="quantity" header="Quantity"></Column>
+    </DataTable>
+  </div>
+</template>
+
+<style></style>
---
+++ components/admin/dashboard/charts/Analytics.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  Analytics
+</template>
+
+<style></style>
