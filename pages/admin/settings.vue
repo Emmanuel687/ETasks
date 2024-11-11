fc40edc9448636de50e34fe80278b2e2360cb49e
Emmanuel687 <koechemmanuel2002@gmail.com>
2024-11-11 21:31:06 +0300

    Chore:-Modified Settings responsiveness in mobile and web

--- pages/admin/settings.vue
+++ pages/admin/settings.vue
@@ -7,14 +7,14 @@ definePageMeta({
 
 
 <template>
-<section class="grid grid-cols-12 gap-[35px]">
+<section class="grid lg:grid-cols-12  gap-[10px] lg:gap-[35px]">
   <!-- Profile Section (No Scroll) -->
-  <div class="col-span-4">
+  <div class="lg:col-span-4">
     <AdminSettingsProfile />
   </div>
 
   <!-- Admin Settings Section (Scrollable) -->
-  <div class="col-span-8  overflow-y-auto">
+  <div class="lg:col-span-8  overflow-y-auto">
     <AdminSettings />
   </div>
 </section>
